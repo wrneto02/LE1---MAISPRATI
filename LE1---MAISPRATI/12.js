fd96534be52e28f64960a76492b72856cfe3afc6
wrneto02 <wiltonrneto@gmail.com>
2025-05-19 15:02:55 -0300

    12.js

--- LE1---MAISPRATI/12.js
+++ LE1---MAISPRATI/12.js
@@ -1,8 +1,9 @@
 //12. Crie um programa que exibe a tabuada de um número fornecido pelo usuário (de 1 a 10) utilizando um loop for.
 
-
+//solicita o número que o usuario deseja ver a tabuada
 let Tabuada = Number(prompt('Digite um número de 1 a 10 para exibir a tabuada: '))
 
+//laço para printar toda a tabuada do numero escolhido
 if(Tabuada >= 1 && Tabuada <= 10){
     console.log(`\nA tabuada do número ${Tabuada} é: `)
     for(let i = 1; i <= 10; i++){ 
